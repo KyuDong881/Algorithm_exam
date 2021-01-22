8e2820551b2e8ef785158fe10690e3cb7392fd84
KyuDong881 <63042112+KyuDong881@users.noreply.github.com>
2021-01-22 22:59:50 +0900

    Create 10211.cpp

---
+++ 빨리풀기/10211.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <string.h>
+#pragma warning(disable 4996)
+using namespace std;
+
+int dp[1001];
+int arr[1001];
+
+int main() {
+	int TC, N, maxSum;
+	scanf("%d", &TC);
+	while (TC--) {
+		memset(dp, 0, sizeof(dp));
+		maxSum = INT_MIN;
+		scanf("%d", &N);
+		for (int i = 1; i <= N; i++) {
+			scanf("%d", &arr[i]);
+		}
+
+
+		for (int i = 1; i <= N; i++) {
+			dp[i] = arr[i];
+			if (arr[i] + dp[i - 1] > dp[i]) {
+				dp[i] = arr[i] + dp[i - 1];
+			}
+			maxSum = max(maxSum, dp[i]);
+		}
+
+		printf("%d\n", maxSum);
+
+	}
+
+	return 0;
+}
